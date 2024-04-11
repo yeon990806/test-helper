d633f1f8cabede28031e043a2cf45a289cc110b8
yeon990806 <yeon990806>
2024-04-11 19:26:38 +0900

    fix: vite config - netlifyPlugin 추가

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { netlifyPlugin } from "@netlify/remix-edge-adapter/plugin";
 
 installGlobals();
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
+  plugins: [remix(),netlifyPlugin(), tsconfigPaths()],
 });
